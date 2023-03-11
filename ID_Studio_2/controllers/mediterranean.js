9ef79d31050c03119a4b92e7ea869830cf0f6160
User Name <user@email.com>
2023-03-11 00:53:18 -0500

    INDUCES routes can be accessed, first model completed

--- ID_Studio_2/controllers/mediterranean.js
+++ ID_Studio_2/controllers/mediterranean.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Mediterranean = require('../models/ideas.js');
+const Mediterranean = require('../models/mediterranean.js');
 
 //ROUTES 
 
@@ -17,6 +17,7 @@ const Mediterranean = require('../models/ideas.js');
 //           description: 'Engineered Wood with Oak Veneer Frame, Metal Handle and Leg',
 //           price: 1199,
 //           tags: 'mediterranean style', 
+//           link: 'https://www.ikea.com/us/en/p/pinnarp-countertop-walnut-veneer-20467479/',
 //         }, 
 //         {
 //               title: '3 Seater Sofa',
@@ -25,6 +26,7 @@ const Mediterranean = require('../models/ideas.js');
 //               description: 'Casual with cylindrical features and curved details, this mid-century modern style sofa brings effortless style to your living space. ',
 //               price: 4999,
 //               tags: 'mediterranean style', 
+//               link: 'https://www.ikea.com/us/en/p/pinnarp-countertop-walnut-veneer-20467479/',
 //             }, 
 
 //       ],
@@ -37,8 +39,10 @@ const Mediterranean = require('../models/ideas.js');
 
 //INDEX
 router.get('/', (req,res) => {
-    Mediterranean.find({}, (error, allMedis)=> {
-        res.render('index2.ejs', {
+    Mediterranean.find({}, (err, allMedis)=> {
+        if(err){console.log(err.message)
+        res.send(err)}
+        res.render('mediterraneanStyle/index.ejs', {
             mediterraneans: allMedis,
         });
     }); 
@@ -47,37 +51,81 @@ router.get('/', (req,res) => {
 
 //NEW
 router.get('/new', (req,res) => {
-
+    res.render('mediterraneanStyle/new.ejs')
 //    res.send('new route')
 });
 
 //DELETE
 router.delete('/:id', (req, res) => {
    //res.send('delete route')
+   Mediterranean.findByIdAndDelete(req.params.id, (err, deletedProduct) => {
+    // findByIdAndDelete product by its ID 
+    if(err) {
+        console.log(err)
+        res.send(err)
+    }
+        //redirect to the index page if the delete is successful 
+        console.log(deletedProduct)
+        res.redirect('/mediterranean')
+})
 });
 
 //UPDATE
 router.put('/:id', (req, res) => {
   // res.send('update route')
-});
+
+
+  Mediterranean.findByIdAndUpdate(req.params.id, req.body, { new: true,}, 
+	(err, updatedProduct) => {
+		// new: true, to get the prodcut after it's been modified 
+		if(err) {
+			console.log(err)
+			res.send(err)
+		}
+			console.log(updatedProduct)
+			// redirect to the index route 
+			res.redirect('/mediterranean')
+		});
+    });
+
 
 //CREATE
 router.post('/', (req, res) => {
     //res.send('create route')
+    Mediterranean.create(req.body, (err, createdMedit )=> {    //the first thing we're going to do is check if there's an error. If there's one, give me the error. If not, run the code 
+        if(err){console.log(err.message)
+            res.send(err)}
+        console.log(createdMedit, "Product created")
+        //res.send(createdMedit); 
+        res.redirect('/mediterranean')
+    });
+
 });
 
 
 //EDIT
 router.get('/:id/edit', (req, res) => { 
-   res.send('route edits')   
-});
+  // res.send('route edits') 
+  Mediterranean.findById(req.params.id, (err, foundProduct) => {
+        if(err){console.log(err.message)
+            res.send(err)}
+          res.render('mediterraneanStyle/edit.ejs', {
+              product: foundProduct 
+          });
+      });
+      });
+  
 
 //SHOW
 router.get('/:id', (req, res) => {
 //    res.send('route shows')
     Mediterranean.findById(req.params.id, (err, foundMedi)=>{
-    res.render('show.ejs', {
-        mediterranean:foundMedi
+        if(err){
+            console.log(err.message)
+            res.send(err)
+        }
+        res.render('mediterraneanStyle/show.ejs', {
+        mediterranean: foundMedi
     });
 });
 });
--- ID_Studio_2/server.js
+++ ID_Studio_2/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 require('dotenv').config();
 const Idea = require('./models/ideas.js');
-const Mediterranean = require('./models/ideas.js');
+const Mediterranean = require('./models/mediterranean.js');
 
 const ideasController = require('./controllers/ideas.js');
 const designsController = require('./controllers/designs.js');
@@ -23,6 +23,7 @@ mongoose.connect(process.env.DATABASE_URL, {
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(methodOverride('_method'))
 app.use('/ideas', ideasController) 
 app.use('/designs', designsController) 
 app.use('/mediterranean', mediterraneanController);
